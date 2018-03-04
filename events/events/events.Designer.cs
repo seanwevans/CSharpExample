98f9903fa3f7048af7772e6fa12bf34693ce8b4f
seanwevans <sean.w.evans@gmail.com>
2018-03-04 18:37:22 -0500

    first

---
+++ events/events/events.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace events
+{
+    partial class events
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblColor = new System.Windows.Forms.Label();
+            this.rdoEnglish = new System.Windows.Forms.RadioButton();
+            this.rdoFrench = new System.Windows.Forms.RadioButton();
+            this.rdoGerman = new System.Windows.Forms.RadioButton();
+            this.rdoItalian = new System.Windows.Forms.RadioButton();
+            this.rdoSpanish = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(383, 9);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(137, 13);
+            this.lblColor.TabIndex = 0;
+            this.lblColor.Text = "Please click on a color chip";
+            // 
+            // rdoEnglish
+            // 
+            this.rdoEnglish.AutoSize = true;
+            this.rdoEnglish.Checked = true;
+            this.rdoEnglish.Location = new System.Drawing.Point(12, 5);
+            this.rdoEnglish.Name = "rdoEnglish";
+            this.rdoEnglish.Size = new System.Drawing.Size(59, 17);
+            this.rdoEnglish.TabIndex = 1;
+            this.rdoEnglish.TabStop = true;
+            this.rdoEnglish.Text = "English";
+            this.rdoEnglish.UseVisualStyleBackColor = true;
+            this.rdoEnglish.Click += new System.EventHandler(this.rdoEnglish_Click);
+            // 
+            // rdoFrench
+            // 
+            this.rdoFrench.AutoSize = true;
+            this.rdoFrench.Location = new System.Drawing.Point(77, 5);
+            this.rdoFrench.Name = "rdoFrench";
+            this.rdoFrench.Size = new System.Drawing.Size(58, 17);
+            this.rdoFrench.TabIndex = 2;
+            this.rdoFrench.Text = "French";
+            this.rdoFrench.UseVisualStyleBackColor = true;
+            this.rdoFrench.Click += new System.EventHandler(this.rdoFrench_Click);
+            // 
+            // rdoGerman
+            // 
+            this.rdoGerman.AutoSize = true;
+            this.rdoGerman.Location = new System.Drawing.Point(141, 5);
+            this.rdoGerman.Name = "rdoGerman";
+            this.rdoGerman.Size = new System.Drawing.Size(62, 17);
+            this.rdoGerman.TabIndex = 3;
+            this.rdoGerman.Text = "German";
+            this.rdoGerman.UseVisualStyleBackColor = true;
+            this.rdoGerman.Click += new System.EventHandler(this.rdoGerman_Click);
+            // 
+            // rdoItalian
+            // 
+            this.rdoItalian.AutoSize = true;
+            this.rdoItalian.Location = new System.Drawing.Point(209, 5);
+            this.rdoItalian.Name = "rdoItalian";
+            this.rdoItalian.Size = new System.Drawing.Size(53, 17);
+            this.rdoItalian.TabIndex = 4;
+            this.rdoItalian.Text = "Italian";
+            this.rdoItalian.UseVisualStyleBackColor = true;
+            this.rdoItalian.Click += new System.EventHandler(this.rdoItalian_Click);
+            // 
+            // rdoSpanish
+            // 
+            this.rdoSpanish.AutoSize = true;
+            this.rdoSpanish.Location = new System.Drawing.Point(268, 5);
+            this.rdoSpanish.Name = "rdoSpanish";
+            this.rdoSpanish.Size = new System.Drawing.Size(63, 17);
+            this.rdoSpanish.TabIndex = 5;
+            this.rdoSpanish.Text = "Spanish";
+            this.rdoSpanish.UseVisualStyleBackColor = true;
+            this.rdoSpanish.Click += new System.EventHandler(this.rdoSpanish_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 599);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ctrl + I : Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 612);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Shift + A : Version";
+            // 
+            // events
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 632);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdoSpanish);
+            this.Controls.Add(this.rdoItalian);
+            this.Controls.Add(this.rdoGerman);
+            this.Controls.Add(this.rdoFrench);
+            this.Controls.Add(this.rdoEnglish);
+            this.Controls.Add(this.lblColor);
+            this.KeyPreview = true;
+            this.Name = "events";
+            this.Text = "Color Translations";
+            this.Load += new System.EventHandler(this.events_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.events_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.events_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.RadioButton rdoEnglish;
+        private System.Windows.Forms.RadioButton rdoFrench;
+        private System.Windows.Forms.RadioButton rdoGerman;
+        private System.Windows.Forms.RadioButton rdoItalian;
+        private System.Windows.Forms.RadioButton rdoSpanish;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ events/events/events.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace events
+{
+    public partial class events : Form
+    {
+        public events()
+        {
+            InitializeComponent();
+        }
+     
+        
+        
+        //
+        // class variables
+        //
+          
+        // languages & default language
+        enum languages { ENGLISH, FRENCH, GERMAN, ITALIAN, SPANISH };
+        int currentLanguage = (int)languages.ENGLISH;
+
+        // default color
+        const string bgColor = "fff2f2f2";  // windows gray
+        Color currentColor = ColorTranslator.FromHtml("#FFF2F2F2");
+        string currentColorName = "fff2f2f2";
+
+        // holds color translation values, referenced by hex code key
+        // colorDict["fff71514"] = { "Red", "Rouge", "Rot", "Rosso", "Rojo" };
+        Dictionary<string, string[]> colorDict = new Dictionary<string, string[]>();
+
+        // background image
+        Bitmap background = new Bitmap("Colors.png");
+        
+        
+        
+        //
+        // form load event
+        //
+                          
+        private void events_Load(object sender, EventArgs e)
+        {            
+            // set background and read in translations to dictionary
+            BackgroundImage = background;
+            string[] colorCodes = File.ReadAllLines("Colors Translated.txt");
+
+            foreach (string s in colorCodes)
+            {
+                string[] spl = s.Split(',');              
+                string[] cols = { spl[1], spl[2], spl[3], spl[4], spl[5] };
+                colorDict.Add(spl[0], cols);
+            }
+        }
+
+        //
+        // form keydown event
+        //
+
+        private void events_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.I)
+            {
+                MessageBox.Show("You can choose from five different languages by clicking on the radio buttons");
+            }
+
+            if (e.Shift && e.KeyCode == Keys.A)
+            {
+                MessageBox.Show("Version 1.0");
+            }
+        }
+
+        //
+        // form mouse down event
+        //
+
+        private void events_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Just click on the color chip to see the translation");
+            }
+            else
+            {
+                UpdateColor(e);
+            }            
+        }
+
+        
+        
+        //
+        // update current color
+        //
+
+        private void UpdateColor(MouseEventArgs e = null)
+        {   
+            // if called from form mousedown event change color
+            if (e != null)
+            {
+                currentColor = background.GetPixel(e.X, e.Y);                
+            }
+            
+            // update color label
+            try
+            {
+                currentColorName = colorDict[currentColor.Name][currentLanguage];
+            }
+            catch (KeyNotFoundException)
+            {
+                currentColorName = colorDict[bgColor][currentLanguage];
+            }
+
+            lblColor.Text = currentColorName;
+        }
+
+
+
+        //
+        // radio button mouse down events
+        // change language and update color
+        //
+
+        private void rdoEnglish_Click(object sender, EventArgs e)
+        {
+            currentLanguage = (int)languages.ENGLISH;
+            UpdateColor();
+        }
+
+        private void rdoFrench_Click(object sender, EventArgs e)
+        {
+            currentLanguage = (int)languages.FRENCH;
+            UpdateColor();
+        }
+
+        private void rdoGerman_Click(object sender, EventArgs e)
+        {
+            currentLanguage = (int)languages.GERMAN;
+            UpdateColor();
+        }
+
+        private void rdoItalian_Click(object sender, EventArgs e)
+        {
+            currentLanguage = (int)languages.ITALIAN;
+            UpdateColor();
+        }
+
+        private void rdoSpanish_Click(object sender, EventArgs e)
+        {
+            currentLanguage = (int)languages.SPANISH;
+            UpdateColor();
+        }                
+    }
+}
